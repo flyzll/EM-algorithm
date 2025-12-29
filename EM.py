56cab8c663610e23d56910947d5a40724de1aedb
flyzll <ryo.tsubo0211@gmail.com>
2025-12-29 17:13:31 +0900

    readme

--- EM.py
+++ EM.py
@@ -11,17 +11,17 @@ def gaussian(x, mu, Sigma):
     diff = x - mu
     inv = np.linalg.inv(Sigma)
     det = np.linalg.det(Sigma)
-    norm = 1.0 / np.sqrt((2 * np.pi)**dim * det)
-    return norm * np.exp(-0.5 * np.sum(diff @ inv * diff, axis=1))
+    norm = 1.0 / np.sqrt((2 * np.pi)**dimention * det)* np.exp(-0.5 * np.sum(diff @ inv * diff, axis=1))
+    return norm 
 
 points = np.vstack([
     generate_object([2, 2, 2],
-                    [[0.3, 0.0, 0.0],
-                     [0.0, 0.3, 0.0],
+                    [[0.3, 0.1, 0.0],
+                     [0.1, 0.3, 0.0],
                      [0.0, 0.0, 0.3]], 300),
 
     generate_object([2, 10, 3],
-                    [[0.3, 0.3, 0.0],
+                    [[0.3, 0.0, 0.0],
                      [0.0, 0.3, 0.0],
                      [0.0, 0.0, 0.3]], 300),
 
@@ -31,22 +31,22 @@ points = np.vstack([
                      [0.0, 0.0, 0.3]], 300)
 ])
 
-N, dim = points.shape
+N, dimention = points.shape
 
 K = 100 #初期クラスタ数
 
 mu = points[np.random.choice(N, K, replace=True)] #平均値
-Sigma = np.array([np.eye(dim) for _ in range(K)]) #共分散
+Sigma = np.array([np.eye(dimention) for _ in range(K)]) #共分散
 pi = np.ones(K) / K  # 混合係数
 
-tol = 1e-3         # 中心変化の許容値
-stable_limit = 3    # 連続回数（満たしたら終了）
+tol = 1e-3         # クラスタ中心の変化の閾値
+stable_limit = 3    #　3回連続で閾値以下なら収束とみなす
 stable_count = 0    # 連続カウンタ
 mu_prev = None
 
 
 for iteration in range(1000):
-    # ---------- E-step ----------
+    # E-step
     gamma = np.zeros((N, K))
     for k in range(K):
         gamma[:, k] = pi[k] * gaussian(points, mu[k], Sigma[k])
@@ -55,7 +55,7 @@ for iteration in range(1000):
     gamma_sum[gamma_sum == 0] = 1e-12
     gamma /= gamma_sum
 
-    # ---------- M-step ----------
+    # M-step
     Nk = gamma.sum(axis=0)
 
     for k in range(K):
@@ -73,7 +73,7 @@ for iteration in range(1000):
         pi[k] = Nk[k] / N
 
     threshold = 0.01   # 全体の1%未満
-    valid = pi > threshold
+    valid = pi > threshold #クラスタが有効かどうかの判定
 
     mu = mu[valid]
     Sigma = Sigma[valid]
@@ -82,20 +82,19 @@ for iteration in range(1000):
     pi /= pi.sum()
     K = len(pi)
 
-    print(f"iter {iteration}: K = {K}")
-
-    # ---------- 収束判定 ----------
+    # 収束判定 
     if mu_prev is not None and mu.shape == mu_prev.shape:
         diff = np.linalg.norm(mu - mu_prev)
 
         if diff < tol:
             stable_count += 1
-            #print(f"  center change small ({stable_count}/{stable_limit})")
+            
         else:
             stable_count = 0
 
         if stable_count >= stable_limit:
-            print("Centers converged. Stop EM.")
+            print(f"iter {iteration}: K = {K}")
+            print("Stop EM.")
             break
 
     mu_prev = mu.copy()
